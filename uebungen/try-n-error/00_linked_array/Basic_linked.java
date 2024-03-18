9ae113c98570e5f308c1033169857da8cdac4139
TomDBaer <thomasbaehren@gmail.com>
2024-03-18 13:26:16 +0100

    add basic linked array

---
+++ uebungen/try-n-error/00_linked_array/Basic_linked.java
@@ -0,0 +1,74 @@
+// Bessere Doku schreiben || UNBEDINGT
+
+// Klasse Auto
+class Auto {
+    String name;
+    Auto next;
+
+    // Konstruktor
+    Auto(String name) {
+        this.name = name;
+        this.next = null;
+    }
+}
+
+// Klasse Liste
+class AutoFabrik {
+    Auto anfang;
+
+    // Konstruktor
+    AutoFabrik() {
+        this.anfang = null;
+    }
+
+    // Methoden
+
+    // hinzufuegen
+    public void add(String name) {
+        var neuesAuto = new Auto(name);
+
+        if (anfang == null) {
+            anfang = neuesAuto;
+            return;
+        }
+
+        /*
+         * Hier wird auf das jetzige Objekt gezeigt.
+         * Solange das nachfolgende Objekt nicht null ist
+         * kommt man nicht in die letzte Zeile, in der man
+         * dann das letzte Objekt, welches null ist, mit einem neuen
+         * Objekt belegt
+         */
+        // momentane Postion im Array
+        var jetzigePosition = anfang;
+        // die jetzige Position +1 ist was?
+        while (jetzigePosition.next != null) {
+            jetzigePosition = jetzigePosition.next;
+        }
+
+        // an die jetzige position ein Objekt setzen
+        jetzigePosition.next = neuesAuto;
+    }
+
+    public void show() {
+        // Anfang der Liste
+        var jetzigePosition = anfang;
+
+        while (jetzigePosition != null) {
+            System.out.println(jetzigePosition.name);
+            jetzigePosition = jetzigePosition.next;
+        }
+    }
+}
+
+public class Basic_linked {
+    public static void main(String[] args) {
+        var neueListe = new AutoFabrik();
+
+        neueListe.add("Audi");
+        neueListe.add("Mercedes");
+        neueListe.add("Toyota");
+
+        neueListe.show();
+    }
+}
